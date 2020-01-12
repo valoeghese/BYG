b207b763b29cb13ec2e4efdae18110783a087a68
AOCAWOL <51396466+AOCAWOL@users.noreply.github.com>
2020-01-12 00:21:04 -0700

    fence, plank, slab, stairs

---
+++ src/main/java/sporeaoc/byg/blocks/StairBlocks.java
@@ -0,0 +1,27 @@
+package sporeaoc.byg.blocks;
+
+import net.minecraft.item.Item;
+
+public class StairBlocks {
+    public static Item ASPEN_STAIRS;
+    public static Item BAOBAB_STAIRS;
+    public static Item BLUE_ENCHANTED_STAIRS;
+    public static Item CHERRY_STAIRS;
+    public static Item CIKA_STAIRS;
+    public static Item CYPRESS_STAIRS;
+    public static Item EBONY_STAIRS;
+    public static Item FIR_STAIRS;
+    public static Item GREEN_ENCHANTED_STAIRS;
+    public static Item HOLLY_STAIRS;
+    public static Item JACARANDA_STAIRS;
+    public static Item MAHOGONY_STAIRS;
+    public static Item MANGROVE_STAIRS;
+    public static Item MAPLE_STAIRS;
+    public static Item PINE_STAIRS;
+    public static Item RAINBOW_EUCALYPTUS_STAIRS;
+    public static Item REDWOOD_STAIRS;
+    public static Item SKYRIS_STAIRS;
+    public static Item WILLOW_STAIRS;
+    public static Item WITCH_HAZEL_STAIRS;
+    public static Item ZELKOVA_STAIRS;
+}
