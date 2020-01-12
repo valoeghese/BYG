32f927842df72b2d1e671923523b0c6151240a85
AOCAWOL <51396466+AOCAWOL@users.noreply.github.com>
2020-01-11 23:16:04 -0700

    Ores & Stones

---
+++ src/main/java/sporeaoc/byg/blocks/OreBlocks.java
@@ -0,0 +1,9 @@
+package sporeaoc.byg.blocks;
+
+import net.minecraft.item.Item;
+
+public class OreBlocks {
+    public static Item PENDORITE_ORE;
+    public static Item TAMRELITE_ORE;
+    public static Item LATHARIUM_ORE;
+}
---
+++ src/main/java/sporeaoc/byg/blocks/StoneBlocks.java
@@ -0,0 +1,10 @@
+package sporeaoc.byg.blocks;
+
+import net.minecraft.item.Item;
+
+public class StoneBlocks {
+    public static Item DACITE_STONE;
+    public static Item SOAPSTONE;
+    public static Item SCORIA_STONE;
+    public static Item SODALITE;
+}
