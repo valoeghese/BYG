a61de95fbd93d6d8f1b3d013f7f0c6dd9bd58e69
FoundSpore <aaronscene349@gmail.com>
2020-01-11 22:22:33 -0800

    Saplings

---
+++ src/main/java/sporeaoc/byg/blocks/SaplingBlocks.java
@@ -0,0 +1,45 @@
+package sporeaoc.byg.blocks;
+
+import net.minecraft.item.Item;
+
+public class SaplingBlocks {
+    public static Item BAOBAB_SAPLING;
+    public static Item BLOOMING_WITCH_HAZEL_SAPLING;
+    public static Item BLUE_ENCHANTED_SAPLING;
+    public static Item BLUE_SPRUCE_SAPLING;
+    public static Item BROWN_BIRCH_SAPLING;
+    public static Item BROWN_OAK_SAPLING;
+    public static Item CIKA_SAPLING;
+    public static Item CYPRESS_SAPLING;
+    public static Item EBONY_SAPLING;
+    public static Item FIR_SAPLING;
+    public static Item FLOWERING_ORCHARD_SAPLING;
+    public static Item FLOWERING_PALO_VERDE_SAPLING;
+    public static Item APPLE_SKYRIS_SAPLING;
+    public static Item GREEN_ENCHANTED_SAPLING;
+    public static Item HOLLY_BETTER_SAPLING;
+    public static Item HOLLY_SAPLING;
+    public static Item JACARANDA_SAPLING;
+    public static Item MAHOGNY_SAPLING;
+    public static Item MANGROVE_SAPLING;
+    public static Item ORANGE_BIRCH_SAPLING;
+    public static Item ORANGE_OAK_SAPLING;
+    public static Item ORANGE_SPRUCE_SAPLING;
+    public static Item ORCHARD_SAPLING;
+    public static Item PALO_VERDE_SAPLING;
+    public static Item PINE_SAPLING;
+    public static Item PINK_CHERRY_SAPLING;
+    public static Item RAINBOW_EUCALYPTUS_SAPLING;
+    public static Item RED_BIRCH_SAPLING;
+    public static Item RED_MAPLE_SAPLING;
+    public static Item RED_OAK_SAPLING;
+    public static Item RED_SPRUCE_SAPLINGS;
+    public static Item REDWOOD_SAPLINGS;
+    public static Item RIPE_ORCHARD_SAPLINGS;
+    public static Item SILVER_MAPLE_SAPLINGS;
+    public static Item SKYRIS_SAPLINGS;
+    public static Item WHITE_CHERRY_SAPLING;
+    public static Item WILLOW_SAPLING;
+    public static Item WITCH_HAZEL_SAPLING;
+    public static Item YELLOW_BIRCH_SAPLING;
+}
