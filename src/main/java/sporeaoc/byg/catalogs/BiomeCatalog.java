bd1dc6620bdc957de89ce77e1ea0083f34a48345
AOCAWOL <51396466+AOCAWOL@users.noreply.github.com>
2020-02-23 14:17:57 -0700

    -Biome Catalog

--- src/main/java/sporeaoc/byg/catalogs/BiomeCatalog.java
+++ src/main/java/sporeaoc/byg/catalogs/BiomeCatalog.java
@@ -21,55 +21,58 @@ public class BiomeCatalog {
     public static Biome CHERRYBLOSSOMFOREST = new CherryBlossomForest();
     public static Biome COASTALREDWOODTROPICS = new CoastalRedwoodTropics();
     public static Biome CONIFEROUSFOREST = new ConiferousForest();
-
-
+    public static Biome DEADSEA = new DeadSea();
     public static Biome DECIDUOUSFOREST = new DeciduousForest();
+    public static Biome DOVERMOUNTAINS = new DoverMountains();
     public static Biome DUNES = new Dunes();
+    public static Biome EBONYWOODS = new EbonyWoods();
+    public static Biome ENCHANTEDFOREST = new EnchantedForest();
+    public static Biome EVERGREENTAIGA = new EvergreenTaiga();
+    public static Biome FLOWERINGPLAINS = new FloweringPlains();
+    public static Biome GLOWSHROOMBAYOU = new GlowShroomBayou();
     public static Biome GRASSLANDPLATEAU = new GrassLandPlateau();
+    public static Biome GREATLAKES = new GreatLakes();
+    public static Biome GUIANASHIELD = new GuianaShield();
     public static Biome JACARANDAFOREST = new JacarandaForest();
+    public static Biome MANGROVEMARSHES = new MangroveMarshes();
+    public static Biome MAPLETAIGA = new MapleTaiga();
+    public static Biome MARSHLANDS = new MarshLands();
+    public static Biome MEADOW = new Meadow();
+    public static Biome ORCHARD = new Orchard();
+    public static Biome PINELOWANDS = new PineLowlands();
+    public static Biome PINEMOUNTAINS = new PineMountains();
     public static Biome PRAIRIE = new Prairie();
     public static Biome QUAGMIRE = new Quagmire();
     public static Biome REDDESERT = new RedDesert();
     public static Biome REDDESERTDUNES = new RedDesertDunes();
     public static Biome REDOAKFOREST = new RedOakForest();
+    public static Biome SAVANNACANOPY = new SavannaCanopy();
     public static Biome SEASONALBIRCHFOREST = new SeasonalBirchForest();
     public static Biome SEASONALDECIDUOUSFOREST = new SeasonalDeciduousForest();
     public static Biome SEASONALFOREST = new SeasonalForest();
     public static Biome SEASONALGIANTTAIGA = new SeasonalGiantTaiga();
     public static Biome SEASONALTAIGA = new SeasonalTaiga();
     public static Biome SHRUBLANDS = new ShrubLands();
+    public static Biome SKYRISHIGHLANDS = new SkyrisHighlands();
     public static Biome SNOWYBLUEGIANTTAIGA = new SnowyBlueGiantTaiga();
     public static Biome SNOWYBLUETAIGA = new SnowyBlueTaiga();
-    public static Biome DEADSEA = new DeadSea();
-    public static Biome DOVERMOUNTAINS = new DoverMountains();
-    public static Biome EBONYWOODS = new EbonyWoods();
-    public static Biome ENCHANTEDFOREST = new EnchantedForest();
-    public static Biome EVERGREENTAIGA = new AlliumFields();
-    public static Biome FLOWERINGPLAINS = new AlliumFields();
-    public static Biome GLOWSHROOMBAYOU = new AlliumFields();
-    public static Biome GREATLAKES = new AlliumFields();
-    public static Biome GUIANASHIELD = new AlliumFields();
-    public static Biome MANGROVEMARSHES = new AlliumFields();
-    public static Biome MAPLETAIGA = new AlliumFields();
-    public static Biome MARSHLANDS = new AlliumFields();
-    public static Biome MEADOW = new AlliumFields();
-    public static Biome ORCHARD = new AlliumFields();
-    public static Biome PINELOWANDS = new AlliumFields();
-    public static Biome PINEMOUNTAINS = new AlliumFields();
-    public static Biome SAVANNACANOPY = new AlliumFields();
-    public static Biome SKYRISHIGHLANDS = new AlliumFields();
-    public static Biome SNOWYCONIFEROUSFOREST = new AlliumFields();
-    public static Biome SNOWYDECIDUOUSFOREST = new AlliumFields();
-    public static Biome SNOWYEVERGREENTAIGA = new AlliumFields();
-    public static Biome SNOWYPINEMOUNTAINS = new AlliumFields();
-    public static Biome SONORANDESERT = new AlliumFields();
-    public static Biome STONEBRUSHLANDS = new AlliumFields();
-    public static Biome TROPICALFUNGALRAINFOREST = new AlliumFields();
-    public static Biome TROPICALISLANDS = new AlliumFields();
-    public static Biome TROPICALRAINFOREST = new AlliumFields();
-    public static Biome WEEPINGWITCHFOREST = new AlliumFields();
-    public static Biome WOODLANDS = new AlliumFields();
-    public static Biome ZELKOVAFOREST = new AlliumFields();
+    public static Biome SNOWYCONIFEROUSFOREST = new SnowyConiferousForest();
+    public static Biome SNOWYDECIDUOUSFOREST = new SnowyDeciduousForest();
+    public static Biome SNOWYEVERGREENTAIGA = new SnowyEvergreenTaiga();
+    public static Biome SNOWYPINEMOUNTAINS = new SnowyPineMountains();
+    public static Biome SONORANDESERT = new SonoranDesert();
+    public static Biome STONEBRUSHLANDS = new StoneBrushlands();
+    public static Biome TROPICALFUNGALRAINFOREST = new TropicalFungalRainForest();
+    public static Biome TROPICALISLANDS = new TropicalIslands();
+    public static Biome TROPICALRAINFOREST = new TropicalRainForest();
+    public static Biome WEEPINGWITCHFOREST = new WeepingWitchForest();
+    public static Biome WOODLANDS = new WoodLands();
+    public static Biome ZELKOVAFOREST = new ZelkovaForest();
+
+
+
+
+
 
 
 }
