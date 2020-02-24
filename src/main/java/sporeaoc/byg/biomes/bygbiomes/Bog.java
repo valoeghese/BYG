d7d534aa8cb61976e144a33a7a2d3c8162a198dc
AOCAWOL <51396466+AOCAWOL@users.noreply.github.com>
2020-02-23 19:35:39 -0700

    -Biome Dictionary pt.2 electric boogaloo

--- src/main/java/sporeaoc/byg/biomes/bygbiomes/Bog.java
+++ src/main/java/sporeaoc/byg/biomes/bygbiomes/Bog.java
@@ -17,7 +17,7 @@ public class Bog extends Biome {
     static final Category CATEGORY = Category.SWAMP;
     static final double DEPTH = 0.02F;
     static final double SCALE = 0.01F;
-    static final float TEMPERATURE = 0.8F;
+    static final float TEMPERATURE = 0.25F;
     static final float DOWNFALL = 0.8F;
     static final int WATER_COLOR = 4159204;
     static final int WATER_FOG_COLOR = 329011;
