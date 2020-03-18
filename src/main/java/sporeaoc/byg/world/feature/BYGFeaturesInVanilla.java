99e4df5ecc6bcda497282ffada51f5e8fa0851b8
FoundSpore <aaronscene349@gmail.com>
2020-03-17 22:24:38 -0700

    added allium flowers to vanilla forest

--- src/main/java/sporeaoc/byg/world/feature/BYGFeaturesInVanilla.java
+++ src/main/java/sporeaoc/byg/world/feature/BYGFeaturesInVanilla.java
@@ -1,16 +1,20 @@
 package sporeaoc.byg.world.feature;
 
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.placement.FrequencyConfig;
+import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
+import sporeaoc.byg.world.feature.biomefeatures.BYGFeatureConfigs;
 
 public class BYGFeaturesInVanilla {
     public void addFeatures(FMLCommonSetupEvent e) {
         for (Biome biome : ForgeRegistries.BIOMES) {
-            if (biome.getCategory() == Biome.Category.PLAINS) {
-
-            }
-            if (biome.getCategory() == Biome.Category.FOREST) {
+            if (biome == Biomes.FOREST) {
+                biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.RANDOM_PATCH.withConfiguration(BYGFeatureConfigs.AMARANTH_CONFIG).withPlacement(Placement.COUNT_HEIGHTMAP_DOUBLE.configure(new FrequencyConfig(1))));
 
             }
         }
