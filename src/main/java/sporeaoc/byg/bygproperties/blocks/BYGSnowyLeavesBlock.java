d933f6a27c2d2e70bb020be99baf544c6773b9e1
AOCAWOL <51396466+AOCAWOL@users.noreply.github.com>
2020-03-19 00:38:51 -0700

    -Added more recipes -Added more loot-tables

--- src/main/java/sporeaoc/byg/bygproperties/blocks/BYGSnowyLeavesBlock.java
+++ src/main/java/sporeaoc/byg/bygproperties/blocks/BYGSnowyLeavesBlock.java
@@ -11,10 +11,7 @@ import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
-import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.IShearable;
-
-import java.util.Random;
 
 public class BYGSnowyLeavesBlock extends LeavesBlock implements IShearable {
 
@@ -32,25 +29,6 @@ public class BYGSnowyLeavesBlock extends LeavesBlock implements IShearable {
             Block block = facingState.getBlock();
             return stateIn.with(SNOWY, Boolean.valueOf(block == Blocks.SNOW_BLOCK || block == Blocks.SNOW));
         }
-    }
-
-    public boolean ticksRandomly(BlockState state) {
-        return state.get(DISTANCE) == 7 && !state.get(PERSISTENT);
-    }
-
-    /**
-     * Performs a random tick on a block.
-     */
-    public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
-        if (!state.get(PERSISTENT) && state.get(DISTANCE) == 7) {
-            spawnDrops(state, worldIn, pos);
-            worldIn.removeBlock(pos, false);
-        }
-
-    }
-
-    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-        worldIn.setBlockState(pos, updateDistance(state, worldIn, pos), 3);
     }
 
     public BlockState getStateForPlacement(BlockItemUseContext context) {
